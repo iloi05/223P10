30bd4b77c187c1d41e2d6a1334a306849417b97a
root <root@DESKTOP-8969P1M.localdomain>
2025-12-04 21:13:28 -0800

    Fixed code, code adds levels now

--- csvcode.py
+++ csvcode.py
@@ -1,39 +1,51 @@
 import csv
 
-counters = {"freshman": 0, "sophomores": 0, "juniors": 0, "seniors": 0}
+counters = {"freshman": 0, "sophomore": 0, "junior": 0, "senior": 0}
 
 students = {}
 
-with open('Students.csv', mode='r') as file1:
+with open('Students.csv', mode='r') as file1, \
+     open('course enrollment.csv', mode = 'r') as file2, \
+     open('outputfile.csv', mode='w', newline='') as file3:
     s = csv.DictReader(file1)
     for row in s:
-        level = row["Level"]
+        level = row["Level"].strip()
+        sid = row["ID"]
 
-        if level in counters:
-            counters[level] += 1
+        if level == "FRESH":
+            counters["freshman"] += 1
+        elif level == "SOPH":
+            counters["sophomore"] += 1
+        elif level == "JR":
+            counters["junior"] += 1
+        elif level == "SR":
+            counters["senior"] += 1
+
+        students[sid] = {"Total_Units": 0, "CPSC_Units": 0}
+
     print(f'{counters["freshman"]} freshmen')
-    print(f'{counters["sophomores"]} sophomores')
-    print(f'{counters["juniors"]} juniors')
-    print(f'{counters["seniors"]} seniors')
+    print(f'{counters["sophomore"]} sophomores')
+    print(f'{counters["junior"]} juniors')
+    print(f'{counters["senior"]} seniors')
 
-with open('course enrollments.csv', mode = 'r') as file2:
     e = csv.DictReader(file2)
     for row in e:
         e_id = row["ID"]
         course = row["Course"]
-        units = row["Units"]
+        units = int(row["Units"])
     
-    if e_id not in students:
-        students[e_id] = {"Total_Units": 0, "CPSC_Units": 0}
+        if e_id in students:
+            students[e_id]["Total_Units"] += units
+            if course.startswith("CPSC"):
+                students[e_id]["CPSC_Units"] += units
+        else:
+            students[e_id] = {
+                "Total_Units": units,
+                "CPSC_Units": units if course.startswith("CPSC") else 0
+            }
 
-        students[e_id]["Total_Units"] += units
+    writer = csv.writer(file3)
+    writer.writerow(["ID", "Total Units", "CPSC Units"])
 
-        if course.startswith("CPSC"):
-            students[e_id]["CPSC_Units"] += units
-
-with open('outputfile.csv', mode='w', newline='') as file3:
-   writer = csv.writer(file3, delimiter=',')
-   writer.writerow(["ID", "Total Units", "CPSC Units"])
-
-   for k, v in students.items():
-        writer.writerow([k, v["Total_Units"], v["CPSC_Units"]])
+    for sid, data in students.items():
+        writer.writerow([sid, data["Total_Units"], data["CPSC_Units"]])
